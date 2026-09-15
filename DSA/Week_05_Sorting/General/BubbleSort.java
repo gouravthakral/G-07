9c9c438ca604090f8315205ed06c55046cd09d24
GOURAV THAKRAL <GOURAVTHAKRAL@Gouravs-MacBook-Air-2.local>
2026-09-15 19:25:33 +0530

    feat(week-05): add bubble sort, selection sort, merge sort, and sorting leetcode problems

---
+++ DSA/Week_05_Sorting/General/BubbleSort.java
@@ -0,0 +1,29 @@
+package DSA.Week_05_Sorting.General;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
